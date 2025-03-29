99b22b0f35727565c29f49e4dc20dce1f3604113
recillah khamala <recillahk@gmail.com>
2025-03-29 16:03:27 +0300

    configure cors to take care of the error

--- config/application.rb
+++ config/application.rb
@@ -28,5 +28,10 @@ module GlovobDemo
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # Add these lines for authentication
+    config.middleware.use ActionDispatch::Flash
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
   end
 end
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,11 +7,13 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:5173' # Your frontend URL
+    origins 'http://localhost:3001', 'http://localhost:5173'  # Allow both common frontend ports
 
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      expose: ['Authorization']
+      expose: ['Authorization'],
+      credentials: true,
+      max_age: 600  # Cache preflight requests for 10 minutes
   end
 end
