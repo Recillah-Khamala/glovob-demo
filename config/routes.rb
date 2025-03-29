a2010bd35b24632584bc7eff1c8ce7b11ecbcfc4
recillah khamala <recillahk@gmail.com>
2025-03-29 05:46:25 +0300

    create the addresses controller for managing user addresses

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,27 @@
 Rails.application.routes.draw do
-  namespace :api do
-    namespace :v1 do
-      get "user_profiles/show"
-      get "user_profiles/update"
-      namespace :auth do
-        get "sessions/create"
-        get "sessions/destroy"
-        get "registrations/create"
-      end
-    end
-  end
-  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+
+  # API Routes
+  namespace :api do
+    namespace :v1 do
+      # Auth routes
+      namespace :auth do
+        post 'signup', to: 'registrations#create'
+        post 'login', to: 'sessions#create'
+        delete 'logout', to: 'sessions#destroy'
+      end
+
+      # User profile routes
+      resource :profile, only: [:show, :update], controller: 'user_profiles'
+
+      # Addresses routes
+      resources :addresses, only: [:index, :create, :update, :destroy]
+    end
+  end
 
   # Defines the root path route ("/")
   # root "posts#index"
