236e70354dd3b557a82653a6b8f0d47fc8689445
recillah khamala <recillahk@gmail.com>
2025-03-29 05:24:18 +0300

    install and run command to use Devise for users authentication

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
+
+  # Associations
+  has_many :addresses, dependent: :destroy
+
+  # Validations
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+end
