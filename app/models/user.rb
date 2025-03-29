6c6eade85b8f253b43c2489ca4cd8204881dd14a
recillah khamala <recillahk@gmail.com>
2025-03-30 01:06:51 +0300

    Include the method to use first name in the User Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
   has_many :addresses, dependent: :destroy
 
   # Validations
-  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :first_name, presence: true
   validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
