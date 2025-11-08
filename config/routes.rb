de7505c017b777b1b35a0f3d87a6112e9cae3e7c
Recillah Khamala <recillahk@gmail.com>
2025-11-08 23:14:18 +0300

    Add API routes for offices, orders, and users endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,16 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  # API routes
+  resources :offices, only: [:index, :show]
+  resources :orders, only: [:create, :show] do
+    member do
+      patch :status, action: :update_status
+    end
+  end
+  resources :users, only: [] do
+    member do
+      get :orders
+    end
+  end
 end
