079c5b79558d5a6ff01542088c3bb14d15b9ca9b
Recillah Khamala <recillahk@gmail.com>
2025-11-09 03:13:45 +0300

    Update CORS configuration to allow frontend origin (localhost:3001)

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,10 +7,12 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "*" # In production, replace with your frontend domain
+    # Development: Allow frontend origin
+    origins Rails.env.development? ? ['http://localhost:3001', 'http://localhost:5173'] : ENV.fetch('FRONTEND_URL', '*')
 
     resource "*",
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: false
   end
 end
